ea9b20d680eec1287498ed58e512f398e68a1c89
Fenriz1349 <129265577+Fenriz1349@users.noreply.github.com>
2024-03-12 15:43:31 +0100

    affichage du calendrier et ajout du package FSCalendar

---
+++ DoItYourPlan/ExtractedViews/DatePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  DatePickerView.swift
+//  DoItYourPlan
+//
+//  Created by apprenant68 on 12/03/2024.
+//
+
+import SwiftUI
+
+struct DatePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DatePickerView()
+}
---
+++ DoItYourPlan/ExtractedViews/FormattedDate.swift
@@ -0,0 +1,18 @@
+//
+//  FormattedDate.swift
+//  DoItYourPlan
+//
+//  Created by apprenant68 on 12/03/2024.
+//
+
+import SwiftUI
+
+struct FormattedDate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FormattedDate()
+}
