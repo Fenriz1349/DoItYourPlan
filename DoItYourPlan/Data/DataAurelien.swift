3954fa363d46520c4f9f97ccd0969911b0c6e28c
Aurélien Chevalier <155169912+Balystick@users.noreply.github.com>
2024-03-14 17:11:24 +0100

    Data ok

--- DoItYourPlan/Data/DataAurelien.swift
+++ DoItYourPlan/Data/DataAurelien.swift
@@ -5,5 +5,53 @@
 //  Created by apprenant68 on 11/03/2024.
 //
 
-import Foundation
 import SwiftUI
+
+class Project: Identifiable, ObservableObject {
+    @Published var id = UUID()
+    @Published var projectName: String
+    @Published var steps: [Step]
+
+    init(projectName: String, steps: [Step]) {
+        self.projectName = projectName
+        self.steps = steps
+    }
+
+    func addStep(stepName: String, orderNumber: Int, isDone: Bool) {
+        let newStep = Step(stepName: stepName, orderNumber: orderNumber, isDone: isDone)
+        steps.append(newStep)
+    }
+
+    func removeStep(at index: Int) {
+        steps.remove(at: index)
+    }
+}
+
+class Step: Identifiable, ObservableObject {
+    @Published var id = UUID()
+    @Published var stepName: String
+    @Published var orderNumber: Int
+    @Published var isDone: Bool
+
+    init(stepName: String, orderNumber: Int, isDone: Bool) {
+        self.stepName = stepName
+        self.orderNumber = orderNumber
+        self.isDone = isDone
+    }
+}
+
+let steps: [Step] = [
+    Step(stepName: "Choix du patron", orderNumber: 1, isDone: false),
+    Step(stepName: "Sélection tissu", orderNumber: 2, isDone: false),
+    Step(stepName: "Mesures", orderNumber: 3, isDone: false),
+    Step(stepName: "Découpe", orderNumber: 4, isDone: false),
+    Step(stepName: "Assemblage", orderNumber: 5, isDone: false),
+    Step(stepName: "Ourlets", orderNumber: 6, isDone: false),
+    Step(stepName: "Ajustements", orderNumber: 7, isDone: false),
+    Step(stepName: "Repassage", orderNumber: 8, isDone: false),
+    Step(stepName: "Finitions", orderNumber: 9, isDone: false)
+]
+
+let myProject = Project(projectName: "Projet de Couture", steps: steps)
+
+
--- DoItYourPlan/Screens/SelectedProjectScreen.swift
+++ DoItYourPlan/Screens/SelectedProjectScreen.swift
@@ -6,42 +6,11 @@
 
 import SwiftUI
 
-class Project: Identifiable, ObservableObject {
-    @Published var id = UUID()
-    @Published var projectName: String
-    
-    init(id: UUID = UUID(), projectName: String) {
-        self.id = id
-        self.projectName = projectName
-    }
-}
-
-class Step: Project {
-    @Published var stepName: String
-    @Published var orderNumber: Int
-    @Published var isDone: Bool
-    
-    init(stepName: String, orderNumber: Int, isDone: Bool, id: UUID = UUID(), projectName: String) {
-            self.stepName = stepName
-            self.orderNumber = orderNumber
-            self.isDone = isDone
-            super.init(id: id, projectName: projectName)
-    }
-}
-
-
 struct SelectedProjectScreen: View {
     @State private var colors: [Color] = [.red, .blue, .purple, .yellow, .black, .indigo, .cyan, .brown, .mint, .orange]
     @State private var draggingItem: Color?
     @State private var zoom: Bool = false
     
-    var steps: [Step] = [
-        Step(stepName: "Choix du patron", orderNumber: 1, isDone: true, projectName: "Projet Couture")]
-        
-    var i: Int = 0
-    //   "Choix du patron", "Sélection tissu", "Mesures", "Découpe", "Assemblage", "Ourlets", "Ajustements", "Repassage", "Finitions"]
-
-        
     var body: some View {
         NavigationStack {
             ScrollView(.vertical) {
@@ -50,42 +19,40 @@ struct SelectedProjectScreen: View {
                         GeometryReader {
                             let size = $0.size
                             NavigationLink(destination: TasksView()) {
-                            HStack {
-                                Text("Description")
-                                ZStack {
-                                    Circle()
-                                        .fill(color.gradient)
-                                    /// Drag
-                                    Text("Etape\r\(color)")
-                                        .foregroundColor(.white)
-                                        .draggable(color) {
-                                            /// Custom Preview View
-                                            Circle()
-                                                .opacity(0.0)
-                                                .onAppear {
-                                                    draggingItem = color
-                                                }
-                                        }
-                                    /// Drop
-                                        .dropDestination(for: Color.self) { items, location in
-                                            draggingItem = nil
-                                            return false
-                                        } isTargeted: { status in
-                                            if let draggingItem, status, draggingItem != color {
-                                                /// Moving Color from source to destination
-                                                if let sourceIndex = colors.firstIndex(of: draggingItem),
-                                                   let destinationIndex = colors.firstIndex(of: color) {
-                                                    withAnimation(.bouncy) {
-                                                        let sourceItem = colors.remove(at: sourceIndex)
-                                                        colors.insert(sourceItem, at: destinationIndex)
-                                                    }
+                                HStack {
+                                    Text(myProject.steps[1].stepName)
+                                    ZStack {
+                                        Circle()
+                                            .fill(myProject.steps[1].isDone ? .gray : .blue)
+                                        Text("Etape\r\(myProject.steps[1].orderNumber)")
+                                            .foregroundColor(.white)
+                                    }
+                                    .draggable(color) {
+                                        Circle()
+                                            .opacity(0.0)
+                                            .onAppear {
+                                                draggingItem = color
+                                            }
+                                    }
+                                    .dropDestination(for: Color.self) { items, location in
+                                        draggingItem = nil
+                                        return false
+                                    } isTargeted: { status in
+                                        if let draggingItem, status, draggingItem != color {
+                                            /// Moving Color from source to destination
+                                            if let sourceIndex = colors.firstIndex(of: draggingItem),
+                                               let destinationIndex = colors.firstIndex(of: color) {
+                                                withAnimation(.bouncy) {
+                                                    let sourceItem = colors.remove(at: sourceIndex)
+                                                    colors.insert(sourceItem, at: destinationIndex)
                                                 }
                                             }
                                         }
-                                }}
-                            .position(x: size.width - 0.5 * size.width * Double.random(in: 0.4...1.0), y: size.height / 2)
-                            
-                        }}
+                                    }
+                                    
+                                }
+                                .position(x: size.width - 0.5 * size.width * Double.random(in: 0.4...1.0), y: size.height / 2)
+                            }}
                         .frame(height: zoom ? 180 : 100)
                     }
                 })
@@ -105,7 +72,7 @@ struct SelectedProjectScreen: View {
                 }
             })
         }
-                
+        
     }
 }
 
