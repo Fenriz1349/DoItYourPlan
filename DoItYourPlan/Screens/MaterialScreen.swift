1e6d27dee93dce8fb5a6adab42c521aecf219d8f
Caroline Jardy <carolinejardy.pro@gmail.com>
2024-03-18 11:23:06 +0100

    materialScreen
    pas de boutions pour rappeler ni rien

--- DoItYourPlan/Screens/MaterialScreen.swift
+++ DoItYourPlan/Screens/MaterialScreen.swift
@@ -8,18 +8,54 @@
 import SwiftUI
 
 struct MaterialScreen: View {
+    @State var materials = ["3 metres de jacquard Rouge","2 metres de ruban dentelle dorée", "3 bobines fil rouge similaire","2 bobines dorées", "Patron Burda Style 6550", "Paire de ciseaux", "Craies/marqueur pour patron", "Aiguilles n°90", "Epingles à tissus"]
+    @State var materialName = ""
+    
+    
     var body: some View {
-        Text("Material screen!")
-//        Tableau ul avec petite coche  pour chaque matériel présent
-//        bouton pour add
-//        “on tap”
-//
-//
-//        sur la nav bar :
-//        titre de l’écran
-//        bouton retour
+        NavigationView{
+            VStack{
+                Form {
+                    Section(header: Text("Ajoutes tout ce dont tu auras besoin pour ce projet")) {
+                        ForEach(materials, id: \.self) { material in
+                            MaterialRow(material: material)
+                        }
+                    }
+                    Section {
+                        Button(action: addMaterial) {
+                            Text("Ajouter un nouveau matériau")
+                        }
+                    }
+                    
+                }
+                .navigationBarTitle("Matériel", displayMode: .inline)
+                .navigationBarItems(leading:
+                                        NavigationLink(destination: SelectedProjectScreen()) {
+                    Text("retour")
+                })
+            }
+        }
     }
-}
+    func addMaterial() {
+         guard !materialName.isEmpty else { return }
+         materials.append(materialName)
+         materialName = ""
+     }
+ }
+
+ struct MaterialRow: View {
+     var material: String
+     
+     var body: some View {
+         HStack {
+             Text(material)
+             Spacer()
+         }
+     }
+ }
+
+
+
 
 #Preview {
     MaterialScreen()
