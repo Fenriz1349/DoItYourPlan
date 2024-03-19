6fcc0a2bc3ac1733e739347ada2dcbc3d512333b
Fenriz1349 <129265577+Fenriz1349@users.noreply.github.com>
2024-03-19 12:56:03 +0100

    Update NewProjectScreen.swift

--- DoItYourPlan/Screens/NewProjectScreen.swift
+++ DoItYourPlan/Screens/NewProjectScreen.swift
@@ -9,17 +9,26 @@ import SwiftUI
 
 struct NewProjectScreen: View {
     
-    @State var projectName = ""
-    @State var selectedOption = NewProjectCategory.couture
-    @State var projectDescription = ""
-    @State var selectedDate: Date = Date()
+    @State private var projectName = ""
+    @State private var selectedOption = NewProjectCategory.couture
+    @State private var projectDescription = ""
+    @State private var selectedDate: Date = Date()
     @Binding var showNewProjet : Bool
+    
+    struct Project {
+        var name: String
+        var category: NewProjectCategory
+        var description: String
+        var deadline: Date
+    }
     var body: some View {
         NavigationView {
-            VStack {
+            NavigationStack {
                 Form {
+                
                     Section(header: Text("Nom du projet")) {
                         TextField("Nom du projet", text: $projectName)
+                            
                     }
                     Section(header: Text("Choisis la catégorie de ton projet DIY")) {
                         Picker("Catégorie:", selection: $selectedOption) {
@@ -28,6 +37,7 @@ struct NewProjectScreen: View {
                                 // Style du menu déroulant: ajouter couleur selon le choix de catégorie
                             }
                         }
+                        .foregroundColor(.black)
                     }
                     Section(header: Text("Détails")) {
                         TextField("Description du projet", text: $projectDescription)
@@ -36,8 +46,8 @@ struct NewProjectScreen: View {
                         DatePicker("Date de fin prévue:", selection: $selectedDate, displayedComponents: [.date])
                     }
                 }
-                NavigationLink(destination: ProjectsScreen(showNewProjet: showNewProjet)
-                    .navigationBarHidden(true), isActive: $showNewProjet) {
+                NavigationLink(destination: ProjectsScreen(showNewProjet: showNewProjet),
+                               isActive: $showNewProjet) {
                     EmptyView()
                 }
 
@@ -50,32 +60,15 @@ struct NewProjectScreen: View {
                         .background(Color.purpleCustom)
                         .cornerRadius(10)
                 }
-                
                 .navigationBarTitle("Nouveau projet", displayMode: .inline)
                 .navigationBarItems(leading:
-                                        NavigationLink(destination: ProjectsScreen(showNewProjet: showNewProjet)) {
+                                        NavigationLink(destination: ProjectsScreen()) {
                     Text("retour")
-                }
-                )
+                })
             }
         }
-        // Bouton "Valider" pour enregistrer le projet
-        
-//        func SaveProject() {
-//            // Action pour enregistrer le projet et naviguer
-//            let newProject = Project(name: projectName, category: selectedOption, description: projectDescription, deadline: selectedDate)
-//            // stocke le nouveau projet
-//
-//            // Une fois que le projet est enregistré, naviguer vers l'écran ProjectScreen
-//            NavigationLink(destination: ProjectsScreen()) {
-//                EmptyView()
-//            }
-//        }
     }
-    
-    
 }
-    
     #Preview {
         NewProjectScreen(showNewProjet: .constant(false))
     }
