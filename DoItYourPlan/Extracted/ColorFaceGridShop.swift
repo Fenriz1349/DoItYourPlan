d22fc3184e907eb9f4054f3a54f8e356304148b5
Fenriz1349 <129265577+Fenriz1349@users.noreply.github.com>
2024-03-19 19:17:50 +0100

    merge Marion
    il manque les assets

--- DoItYourPlan/Extracted/ColorFaceGridShop.swift
+++ DoItYourPlan/Extracted/ColorFaceGridShop.swift
@@ -1,62 +1,85 @@
-
-/*import SwiftUI
+import SwiftUI
 
 struct ColorFaceGridShop: View {
     @Binding var selectedColorFace: ColorFace?
     @Binding var userPoints: Int
     @Binding var showAlert: Bool
-    var colorFaces: [ColorFace]
+    @ObservedObject var userSelections: UserSelections
     
     var body: some View {
-            ScrollView {
-                Text("Couleur de peau")
-                    .font(.system(size: 24))
-                    .fontWeight(.bold)
-                    .padding(.bottom, 20)
-                LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 3), spacing: 20) {
-                    ForEach(colorFaces) { colorFace in
-                        VStack {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(colorFace.color) // Utilisez la couleur directement
-                                    .frame(width: 90, height: 90)
-                                    .overlay(
-                                        colorFace.iconCadenas ? Image(systemName: "lock.fill").foregroundColor(.black) : nil
-                                    )
-                            }
-                            .onTapGesture {
-                                if colorFace.iconCadenas && userPoints >= colorFace.points?.nbrPoints ?? 0 {
-                                    userPoints -= colorFace.points?.nbrPoints ?? 0
-                                    if let index = colorFaces.firstIndex(where: { $0.id == colorFace.id }) {
-                                        // Mis à jour pour refléter le déverrouillage dans le modèle de données
-                                        // Note : cela nécessite que colorFaces soit mutable et observable
-                                    }
-                                    selectedColorFace = colorFace
-                                } else if !colorFace.iconCadenas {
-                                    selectedColorFace = colorFace
-                                } else {
-                                    showAlert = true
-                                }
-                            }
-
-                            if colorFace.iconCadenas, let points = colorFace.points {
-                                HStack {
-                                    Image(systemName: points.iconPoints) // Assurez-vous que ceci est un symbole SF valide
+        RoundedRectangle(cornerRadius: 0)
+            .fill(Color.black)
+            .frame(height: 1)
+        ScrollView {
+            Text("Couleurs de peau")
+                .font(.system(size: 24))
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 20), count: 3), spacing: 20) {
+                ForEach(colorFaces) { colorFace in
+                    VStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(colorFace.color)
+                                .frame(width: 90, height: 90)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 20)
+                                        .stroke(Color.black, lineWidth: 1)
+                                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                                )
+                            VStack {
+                                if colorFace.iconCadenas {
+                                    Image(systemName: "lock.fill")
                                         .foregroundColor(.black)
-                                    Text("\(points.nbrPoints)")
+                                        .padding(.top, 15)
+                                        .padding(.leading, 40)
+                                } else {
+                                    Color.clear // Vue vide pour maintenir l'espace
+                                        .frame(width: 24, height: 24) // Ajustez la taille au besoin pour correspondre à l'icône
+                                        .padding(.top, -37)
+                                        .padding(.trailing, 40)
                                 }
+                                Spacer() // Assure que l'espace réservé ou l'icône soit toujours en haut
+                            }
+                        }
+                        if let points = colorFace.points {
+                            HStack {
+                                Image(systemName: "leaf.fill")
+                                    .foregroundColor(.black)
+                                Text("\(points.nbrPoints)")
+                                    .foregroundColor(.black)
+                            }
+                        } else {
+                            // Gardez un espace pour l'alignement même quand il n'y a pas de points
+                            HStack {
+                                Spacer()
+                                Text(" ")
+                                    .foregroundColor(.clear)
                             }
                         }
                     }
+                    .onTapGesture {
+                        if colorFace.iconCadenas {
+                            if userPoints >= colorFace.points?.nbrPoints ?? 0 {
+                                userPoints -= colorFace.points?.nbrPoints ?? 0
+                                userSelections.selectedColorFace = colorFace
+                            } else {
+                                showAlert = true
+                            }
+                        } else {
+                            userSelections.selectedColorFace = colorFace
+                        }
+                    }
                 }
-                .padding()
             }
+            .padding()
         }
     }
+}
+
 #Preview{
   
-    ColorFaceGridShop(selectedColorFace: .constant(nil), userPoints: .constant(100), showAlert: .constant(false))
+    ColorFaceGridShop(selectedColorFace: .constant(nil), userPoints: .constant(100), showAlert: .constant(false),  userSelections: userSelections)
       }
 
-    */
-
+    
